f082201e90c08220d007beb722261674c1037b7f
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-05 22:24:34 +0100

    add types

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,11 +1,24 @@
-
-export interface Rating {
-    rate: number;
-    count: number;
+export type APIResponse<T> = {
+    results: T;
+    data: T;
+    status: ResponseStatus;
 }
 
-export interface Product {
+enum ResponseStatus {
+    Success = "success",
+    Error = "error",
+}
+
+export type Product = {
     id: number;
     title: string;
     price: number;
+}
+
+export type User = {
+    name: {
+        title: string;
+        first: string;
+        last: string;
+    }
 }
