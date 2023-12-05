20036afe7d5019959aeb408b0294eebd517066bb
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-05 22:25:40 +0100

    add fetch and typing to functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { getAllProducts } from './client/GetAllProducts';
-import { Product } from './client/types';
-import { ProductList } from './ProductList';
-import { Title } from './Title';
-import { ListSelect } from './ListSelect';
+import { fetchData } from './client/fetchData';
+import { Product, User } from './client/types';
+// import { ProductList } from './ProductList';
+// import { Title } from './Title';
+// import { ListSelect } from './ListSelect';
 import Card from './Card';
 
 const App = () => {
+  const [usersList, setUsersList] = useState<User[]>([]);
+  const [productsList, setProductsList] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchAndSetData = async () => {
+      const users = await fetchData<User[]>('https://randomuser.me/api/?results=10');
+      const productDetails = await fetchData<Product[]>('https://fakestoreapi.com/products');
+
+      console.log("Users:", users?.results.map(user => user.name.first))
+      console.log("Products:", productDetails)
+
+      setProductsList(productDetails);
+      setUsersList(users?.results);
+    }
+    
+    fetchAndSetData();
+  }, [])
+  
+
   return <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
     <Card>
       <Card.Header>Kontaktinformation</Card.Header>
@@ -14,6 +33,11 @@ const App = () => {
         <ul>
           <li>Jonatan</li>
           <li>070-123 123 12</li>
+          {usersList.map(user => 
+            (
+              <li>{user.name.first} {user.name.last}</li>
+            )
+          )}
         </ul>
       </Card.Body>
     </Card>
@@ -23,6 +47,11 @@ const App = () => {
         <ul>
           <li>Jonatan</li>
           <li>070-123 123 12</li>
+          {productsList.map(product => 
+            (
+              <li>{product.title} {product.price}</li>
+            )
+          )}
         </ul>
       </Card.Body>
     </Card>
