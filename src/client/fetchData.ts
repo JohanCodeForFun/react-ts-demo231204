32d0edddd83df2403892b46d47cc0527a125eab6
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-06 11:44:26 +0100

    add data type to response

--- src/client/fetchData.ts
+++ src/client/fetchData.ts
@@ -2,6 +2,6 @@ import { APIResponse } from "./types";
 
 export const fetchData = async <T>(url: string): Promise<APIResponse<T>> => {
   const response = await fetch(url);
-  const data = await response.json()
-  return data as APIResponse<T>;
+  const data: APIResponse<T> = await response.json()
+  return data;
 }
