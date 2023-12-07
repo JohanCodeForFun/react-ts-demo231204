a0a6509216dcc2cb39d14554b7ce5651ea26527e
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-07 15:04:55 +0100

    format file and add key index

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,13 +1,16 @@
 import { SelectUsers } from "./client/types"
 
 export const Select = ({ options, onChange } :
-  {options: SelectUsers<number>[], onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void}) => {
-  
-  return (
+  {
+    options: SelectUsers<number>[],
+    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
+  }) => {
+
+    return (
   <div>
     <select onChange={onChange}>
       {options.map(user => (
-        <option value={user.value}>
+        <option value={user.value} key={user.value}>
           {user.label}
         </option>
       ))}
